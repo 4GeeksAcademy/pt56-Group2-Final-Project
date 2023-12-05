566fc2f1c0462e8b0bbe76e24f1f5f556494c4c4
Bhooker88 <139884664+Bhooker88@users.noreply.github.com>
2023-12-05 18:12:26 +0000

    fixed

--- src/front/js/pages/MyPosts.jsx
+++ src/front/js/pages/MyPosts.jsx
@@ -1,36 +1,46 @@
-import React, {useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { Context } from "../store/appContext";
 
 const MyPosts = () => {
   const { store, actions } = useContext(Context);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     // Fetch posts when the component mounts
-    actions.getPosts();
-  }, []);
+    actions.getPosts().then(() => {
+      setIsLoading(false);
+    });
+  }, [actions]);
 
-  console.log("Posts in store:", store.posts); // Add this line to check the posts in the console
+  console.log("Posts in store:", store.posts);
 
   return (
     <div className="container mt-4">
       <h1 className="text-center mb-4">My Posts</h1>
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <ul className="list-group">
-            {store.posts.map((post) => (
-              <li key={post.id} className="list-group-item">
-                <h3>{post.location}</h3>
-                <p>Stay: {post.stay}</p>
-                <p>Food: {post.food}</p>
-                <p>Activities: {post.activities}</p>
-                <p>Transportation: {post.transportation}</p>
-                <p>Comments: {post.comments}</p>
-                {post.media && <img src={post.media} alt="Post Media" className="img-fluid" />}
-              </li>
-            ))}
-          </ul>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <ul className="list-group">
+              {store.posts.map((post) => (
+                <li key={post.id} className="list-group-item">
+                  <h3>Location: {post.place_name}</h3>
+                  <p>Stay: {post.stay}</p>
+                  <p>Food: {post.food_drinks}</p>
+                  <p>Activities: {post.activities}</p>
+                  <p>Transportation: {post.transportation}</p>
+                  <p>Tips: {post.tips}</p>
+                  {/* Add any additional fields you want to display */}
+                  {post.media && (
+                    <img src={post.media} alt="Post Media" className="img-fluid" />
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
